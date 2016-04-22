9b4ceffb83daff0e27fbcd54e667e136b7b712ea
U-ITG\jafris <jafris@ITGL-2TBRN72.itg.com>
2016-04-22 10:56:26 -0400

    Copied then modified script to handle osx or cygwin

---
+++ symlinkify.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+
+osxfiles="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+
+cygwinfiles="tmux.conf bash_profile minttyrc bashrc gradle"
+
+if [ $1 = "cygwin" ]; then
+  files=cygwinfiles
+else [ $1 = "osx" ]; then
+  files=osxfiles
+else
+  echo "no argument specified. Please specify osx or cygwin (windows)"
+  exit 1
+fi
+
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
